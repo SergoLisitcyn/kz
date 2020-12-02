c257edadf1aa12fd93fbe56376e56dccd1aadac3
sergo <sergolisitcyn@gmail.com>
2020-12-02 21:04:12 +0200

    fix

--- frontend/views/layouts/main.php
+++ frontend/views/layouts/main.php
@@ -22,6 +22,28 @@ AppAsset::register($this);
     <?php $this->registerCsrfMetaTags() ?>
     <title><?= Html::encode($this->title) ?></title>
     <?php $this->head() ?>
+    <!-- Yandex.Metrika counter -->
+    <script type="text/javascript" >
+        (function(m,e,t,r,i,k,a){m[i]=m[i]||function(){(m[i].a=m[i].a||[]).push(arguments)};
+            m[i].l=1*new Date();k=e.createElement(t),a=e.getElementsByTagName(t)[0],k.async=1,k.src=r,a.parentNode.insertBefore(k,a)})
+        (window, document, "script", "https://mc.yandex.ru/metrika/tag.js", "ym");
+
+        ym(56286430, "init", {
+            clickmap:true,
+            trackLinks:true,
+            accurateTrackBounce:true
+        });
+    </script>
+    <noscript><div><img src="https://mc.yandex.ru/watch/56286430" style="position:absolute; left:-9999px;" alt="" /></div></noscript>
+    <script async src="https://www.googletagmanager.com/gtag/js?id=UA-152813302-1"></script>
+    <script>
+        window.dataLayer = window.dataLayer || [];
+        function gtag(){dataLayer.push(arguments);}
+        gtag('js', new Date());
+
+        gtag('config', 'UA-152813302-1');
+    </script>
+
 </head>
 
 <body>
